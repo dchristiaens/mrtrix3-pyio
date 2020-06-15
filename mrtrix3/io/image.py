7a2911e89570d4c9b6550631f04664e70cb37f8f
Max Pietsch <mail@maxpietsch.com>
2020-06-15 11:24:08 +0200

    image comment unmutable default argument

--- mrtrix3/io/image.py
+++ mrtrix3/io/image.py
@@ -30,12 +30,12 @@ class Image (object):
 
     def __init__(self, data=None, vox=(),
                        transform=np.eye(4),
-                       grad=None, comments=[]):
+                       grad=None, comments=None):
         self.data = data
         self.vox = vox
         self.transform = transform
         self.grad = grad
-        self.comments = comments
+        self.comments = comments if comments is not None else []
 
 
     _array_attr = ['shape', 'ndim', 'dtype', 'size', 'strides', 'nbytes']
