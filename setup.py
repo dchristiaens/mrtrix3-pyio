c72dd5011e573436556fb0266ed46fd436a1be93
Daan Christiaens <daan.christiaens@gmail.com>
2023-05-17 17:16:34 +0200

    Fix package version in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mrtrix3',
-      version='py-0.0.1',
+      version='1.0.0',
       description='Python package to interface with MRtrix3 file formats.',
       url='http://github.com/dchristiaens/mrtrix3-pyio.git',
       author='Daan Christiaens',
